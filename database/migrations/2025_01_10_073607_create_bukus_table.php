023a9c80781f570614f45ef8416badea0528580a
pajargit <elboyganteng972@gmail.com>
2025-01-10 14:52:52 +0700

    commit pertama, buat model dan migrations

---
+++ database/migrations/2025_01_10_073607_create_bukus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bukus', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("Kategori_id")->constrained("kategoris");
+            $table->string("judul");
+            $table->string("Penulis");
+            $table->string("Penerbit");
+            $table->int("Tahun_terbit");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bukus');
+    }
+};
