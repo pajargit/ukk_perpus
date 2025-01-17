589f4ba51ca1a2145d3681044e0d308eaf43408d
pajargit <elboyganteng972@gmail.com>
2025-01-17 13:28:26 +0700

    schema db clear

--- database/migrations/2025_01_16_031036_create_pinjams_table.php
+++ database/migrations/2025_01_16_031036_create_pinjams_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('bukus', function (Blueprint $table) {
+        Schema::create('pinjams', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("Kategori_id")->constrained("kategoris");
-            $table->string("kode_buku");
-            $table->string("judul");
-            $table->string("Penulis");
-            $table->string("Penerbit");
-            $table->int("Tahun_terbit");
+            $table->foreignid('user_id')->constrained('users');
+            $table->foreignid('buku_id')->constrained('bukus');
+            $table->date('tgl_pinjam');
+            $table->date('tgl_kembali');
+            $table->varchar('status')->deafult('null');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('bukus');
+        Schema::dropIfExists('pinjams');
     }
 };
--- database/migrations/2025_01_16_031719_create_ulasans_table.php
+++ database/migrations/2025_01_16_031719_create_ulasans_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('bukus', function (Blueprint $table) {
+        Schema::create('ulasans', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("Kategori_id")->constrained("kategoris");
-            $table->string("kode_buku");
-            $table->string("judul");
-            $table->string("Penulis");
-            $table->string("Penerbit");
-            $table->int("Tahun_terbit");
+            $table->foreignid('user_id')->constrained('users')
+            $table->foreignid('buku_id')->constrained('bukus')
+            $table->text('ulasan');
+            $table->integer('rating');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('bukus');
+        Schema::dropIfExists('ulasans');
     }
 };
